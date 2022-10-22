6b8175e9ece8cd4f29de9c318dcc1fce47f8cf4d
Mateusz Jurczak <jurczak.mateusz@gmail.com>
2022-10-22 16:05:19 +0200

    Initial

---
+++ UT/test1.hpp
@@ -0,0 +1,18 @@
+/*
+ * test1.hpp
+ *
+ *  Created on: 22 paź 2022
+ *      Author: Mateusz
+ */
+
+#ifndef TEST1_HPP_
+#define TEST1_HPP_
+#include <stdlib.h>
+
+uint8_t test_return1(void)
+{
+	return 1;
+}
+
+
+#endif /* TEST1_HPP_ */
---
+++ src/PSES.cpp
@@ -0,0 +1,21 @@
+//============================================================================
+// Name        : PSES.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+#include "catch.hpp"
+#include "test1.hpp"
+using namespace std;
+
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	TEST_CASE(
+			)
+	return EXIT_SUCCESS;
+}
