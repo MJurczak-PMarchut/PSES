ce71d50397e9a16e3891b3a70eedc93b92a92df4
Mateusz Jurczak <jurczak.mateusz@gmail.com>
2023-01-07 13:58:02 +0100

    Initial

---
+++ Autosar/include/CanTP.h
@@ -0,0 +1,50 @@
+/*
+ * CanTP.h
+ *
+ *  Created on: 1 sty 2023
+ *      Author: Mateusz
+ */
+
+#ifndef INCLUDE_CANTP_H_
+#define INCLUDE_CANTP_H_
+
+#include "CanTP_Types.h"
+#define _VENDOR_ID 0
+#define _MODULE_ID 0
+#define _INSTANCE_ID 0
+
+#define CANTP_SW_MAJOR_VERSION    	0
+#define CANTP_SW_MINOR_VERSION 		0
+#define CANTP_SW_PATCH_VERSION    	0
+#define CANTP_AR_MAJOR_VERSION    	4
+#define CANTP_AR_MINOR_VERSION    	4
+#define CANTP_AR_PATCH_VERSION    	0
+
+typedef struct {
+
+}CanTp_ConfigType;
+
+/*
+ * @brief
+ * Service ID: 0x01
+ * This function initializes the CanTp module
+ */
+void CanTp_Init (const CanTp_ConfigType* CfgPtr);
+
+/*
+ * @brief
+ * Service ID: 0x07
+ * This function returns the version information of the CanTp module
+ */
+void CanTp_GetVersionInfo ( Std_VersionInfoType* versioninfo);
+
+
+/*
+ * @brief
+ * Service ID: 0x02
+ * This function is called to shutdown the CanTp module.
+ */
+void CanTp_Shutdown (void);
+
+
+#endif /* INCLUDE_CANTP_H_ */
---
+++ Autosar/include/CanTP_Types.h
@@ -0,0 +1,20 @@
+/*
+ * CanTP_Types.h
+ *
+ *  Created on: 1 sty 2023
+ *      Author: Mateusz
+ */
+
+#ifndef INCLUDE_CANTP_TYPES_H_
+#define INCLUDE_CANTP_TYPES_H_
+
+#include "Std_Types.h"
+#include "Platform_Types.h"
+
+typedef enum {
+	CANTP_OFF, CANTP_ON
+} CanTp_StateType;
+
+
+
+#endif /* INCLUDE_CANTP_TYPES_H_ */
