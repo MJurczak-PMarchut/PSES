93a7963f99d8525d46da60452f38ab88f62f46a3
PaulinaMarchut <paulina8314@gmail.com>
2023-01-28 21:09:52 +0100

    create test_CanTp_FirstFrameReceived

--- UT/UT_CanTP.cpp
+++ UT/UT_CanTP.cpp
@@ -18,6 +18,7 @@
 
 DEFINE_FFF_GLOBALS;
 
+
 FAKE_VOID_FUNC(PduR_CanTpRxIndication, PduIdType, Std_ReturnType);
 
 
@@ -51,78 +52,78 @@ void test_CanTp_GetVersionInfo(void)
 
 void test_CanTp_GetPCI(void)
 {
-//	PduInfoType PduInfoPtr;
-//	uint8_t SduData[6] = {0};
-//	CanPCI_Type CanPCI = {0};
-//	Std_ReturnType ret = E_NOT_OK;
-//	PduInfoPtr.SduDataPtr = SduData;
-//	// FirstFrame, FF_DL > 4095
-//	SduData[0] = (FirstFrame << 4) + 0x0;
-//	SduData[1] = 0x00;
-//	SduData[2] = 0xF0;
-//	SduData[3] = 0xF0;
-//	SduData[4] = 0xF0;
-//	SduData[5] = 0xF0;
-//	ret = CanTp_GetPCI(&PduInfoPtr, &CanPCI);
-//	TEST_CHECK(ret == E_OK);
-//	TEST_CHECK(CanPCI.FrameType == FirstFrame);
-//	TEST_CHECK(CanPCI.FrameLength == 0xF0F0F0F0);
-//	// FirstFrame, FF_DL = 4095
-//	SduData[0] = (FirstFrame << 4) + 0xF;
-//	SduData[1] = 0xFF;
-//	ret = CanTp_GetPCI(&PduInfoPtr, &CanPCI);
-//	TEST_CHECK(ret == E_OK);
-//	TEST_CHECK(CanPCI.FrameType == FirstFrame);
-//	TEST_CHECK(CanPCI.FrameLength == 0xFFF);
-//	// FirstFrame, FF_DL < 4095
-//	SduData[0] = (FirstFrame << 4) + 0xF;
-//	SduData[1] = 0xFE;
-//	ret = CanTp_GetPCI(&PduInfoPtr, &CanPCI);
-//	TEST_CHECK(ret == E_OK);
-//	TEST_CHECK(CanPCI.FrameType == FirstFrame);
-//	TEST_CHECK(CanPCI.FrameLength == 0xFFE);
-//	// SingleFrame, CAN_DL > 8
-//	SduData[0] = (SingleFrame << 4) + 0x0;
-//	SduData[1] = 0xFE;
-//	ret = CanTp_GetPCI(&PduInfoPtr, &CanPCI);
-//	TEST_CHECK(ret == E_OK);
-//	TEST_CHECK(CanPCI.FrameType == SingleFrame);
-//	TEST_CHECK(CanPCI.FrameLength == 0xFE);
-//	// SingleFrame, CAN_DL = 8
-//	SduData[0] = (SingleFrame << 4) + 0x8;
-//	SduData[1] = 0xFE;
-//	ret = CanTp_GetPCI(&PduInfoPtr, &CanPCI);
-//	TEST_CHECK(ret == E_OK);
-//	TEST_CHECK(CanPCI.FrameType == SingleFrame);
-//	TEST_CHECK(CanPCI.FrameLength == 0x8);
-//	// SingleFrame, CAN_DL < 8
-//	SduData[0] = (SingleFrame << 4) + 0x2;
-//	SduData[1] = 0xFE;
-//	ret = CanTp_GetPCI(&PduInfoPtr, &CanPCI);
-//	TEST_CHECK(ret == E_OK);
-//	TEST_CHECK(CanPCI.FrameType == SingleFrame);
-//	TEST_CHECK(CanPCI.FrameLength == 0x2);
-//	// FlowControlFrame
-//	SduData[0] = (FlowControlFrame << 4) + 0x1;
-//	SduData[1] = 0x02;
-//	SduData[2] = 0x03;
-//	ret = CanTp_GetPCI(&PduInfoPtr, &CanPCI);
-//	TEST_CHECK(ret == E_OK);
-//	TEST_CHECK(CanPCI.FrameType == FlowControlFrame);
-//	TEST_CHECK(CanPCI.FS == 0x01);
-//	TEST_CHECK(CanPCI.BS == 0x02);
-//	TEST_CHECK(CanPCI.ST == 0x03);
-//	// ConsecutiveFrame
-//	SduData[0] = (ConsecutiveFrame << 4) + 0x1;
-//	ret = CanTp_GetPCI(&PduInfoPtr, &CanPCI);
-//	TEST_CHECK(ret == E_OK);
-//	TEST_CHECK(CanPCI.FrameType == ConsecutiveFrame);
-//	TEST_CHECK(CanPCI.SN == 0x01);
-//	// IncorrectFrame
-//	SduData[0] = (0xF << 4) + 0x1;
-//	ret = CanTp_GetPCI(&PduInfoPtr, &CanPCI);
-//	TEST_CHECK(ret == E_NOT_OK);
-//	TEST_CHECK(CanPCI.FrameType == (CanTPFrameType)-1);
+	PduInfoType PduInfoPtr;
+	uint8_t SduData[6] = {0};
+	CanPCI_Type CanPCI = {0};
+	Std_ReturnType ret = E_NOT_OK;
+	PduInfoPtr.SduDataPtr = SduData;
+	// FirstFrame, FF_DL > 4095
+	SduData[0] = (FirstFrame << 4) + 0x0;
+	SduData[1] = 0x00;
+	SduData[2] = 0xF0;
+	SduData[3] = 0xF0;
+	SduData[4] = 0xF0;
+	SduData[5] = 0xF0;
+	ret = CanTp_GetPCI(&PduInfoPtr, &CanPCI);
+	TEST_CHECK(ret == E_OK);
+	TEST_CHECK(CanPCI.FrameType == FirstFrame);
+	TEST_CHECK(CanPCI.FrameLength == 0xF0F0F0F0);
+	// FirstFrame, FF_DL = 4095
+	SduData[0] = (FirstFrame << 4) + 0xF;
+	SduData[1] = 0xFF;
+	ret = CanTp_GetPCI(&PduInfoPtr, &CanPCI);
+	TEST_CHECK(ret == E_OK);
+	TEST_CHECK(CanPCI.FrameType == FirstFrame);
+	TEST_CHECK(CanPCI.FrameLength == 0xFFF);
+	// FirstFrame, FF_DL < 4095
+	SduData[0] = (FirstFrame << 4) + 0xF;
+	SduData[1] = 0xFE;
+	ret = CanTp_GetPCI(&PduInfoPtr, &CanPCI);
+	TEST_CHECK(ret == E_OK);
+	TEST_CHECK(CanPCI.FrameType == FirstFrame);
+	TEST_CHECK(CanPCI.FrameLength == 0xFFE);
+	// SingleFrame, CAN_DL > 8
+	SduData[0] = (SingleFrame << 4) + 0x0;
+	SduData[1] = 0xFE;
+	ret = CanTp_GetPCI(&PduInfoPtr, &CanPCI);
+	TEST_CHECK(ret == E_OK);
+	TEST_CHECK(CanPCI.FrameType == SingleFrame);
+	TEST_CHECK(CanPCI.FrameLength == 0xFE);
+	// SingleFrame, CAN_DL = 8
+	SduData[0] = (SingleFrame << 4) + 0x8;
+	SduData[1] = 0xFE;
+	ret = CanTp_GetPCI(&PduInfoPtr, &CanPCI);
+	TEST_CHECK(ret == E_OK);
+	TEST_CHECK(CanPCI.FrameType == SingleFrame);
+	TEST_CHECK(CanPCI.FrameLength == 0x8);
+	// SingleFrame, CAN_DL < 8
+	SduData[0] = (SingleFrame << 4) + 0x2;
+	SduData[1] = 0xFE;
+	ret = CanTp_GetPCI(&PduInfoPtr, &CanPCI);
+	TEST_CHECK(ret == E_OK);
+	TEST_CHECK(CanPCI.FrameType == SingleFrame);
+	TEST_CHECK(CanPCI.FrameLength == 0x2);
+	// FlowControlFrame
+	SduData[0] = (FlowControlFrame << 4) + 0x1;
+	SduData[1] = 0x02;
+	SduData[2] = 0x03;
+	ret = CanTp_GetPCI(&PduInfoPtr, &CanPCI);
+	TEST_CHECK(ret == E_OK);
+	TEST_CHECK(CanPCI.FrameType == FlowControlFrame);
+	TEST_CHECK(CanPCI.FS == 0x01);
+	TEST_CHECK(CanPCI.BS == 0x02);
+	TEST_CHECK(CanPCI.ST == 0x03);
+	// ConsecutiveFrame
+	SduData[0] = (ConsecutiveFrame << 4) + 0x1;
+	ret = CanTp_GetPCI(&PduInfoPtr, &CanPCI);
+	TEST_CHECK(ret == E_OK);
+	TEST_CHECK(CanPCI.FrameType == ConsecutiveFrame);
+	TEST_CHECK(CanPCI.SN == 0x01);
+	// IncorrectFrame
+	SduData[0] = (0xF << 4) + 0x1;
+	ret = CanTp_GetPCI(&PduInfoPtr, &CanPCI);
+	TEST_CHECK(ret == E_NOT_OK);
+	TEST_CHECK(CanPCI.FrameType == (CanTPFrameType)-1);
 }
 
 void test_CanTp_RxIndication(void)
@@ -178,7 +179,7 @@ void test_CanTp_RxIndication(void)
 
 void test_CanTp_RxIndicationHandleSuspendedState(void)
 {
-	PduIdType RxPduId;
+	PduIdType RxPduId = 1;
 	PduInfoType PduInfo;
 	CanPCI_Type Can_PCI;
 	RESET_FAKE(PduR_CanTpRxIndication);
@@ -189,6 +190,13 @@ void test_CanTp_RxIndicationHandleSuspendedState(void)
 	TEST_CHECK(PduR_CanTpRxIndication_fake.call_count == 1);
 	TEST_CHECK(CanTP_State.RxState.CanTp_RxState == CANTP_RX_WAIT);
 
+}
+
+void test_CanTp_FirstFrameReceived(void)
+{
+	PduIdType RxPduId;
+	PduInfoType PduInfoPtr;
+	CanPCI_Type *Can_PCI;
 }
 
 #endif /* UT_CANTP_CPP_ */
